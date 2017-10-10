ba77289d443839f7538c09179fb431f0af2634c3
tauomicronmu <tauomicronmurs@gmail.com>
2017-10-10 15:51:15 +0100

    sorted server

--- server.c
+++ server.c
@@ -13,8 +13,8 @@
 int main(int argc, char ** argv) {
 
     int portno, sockfd;
-    struct sockaddr_in6 serv_addr;
-    struct sockaddr_in6 cli_addr;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in cli_addr;
 
     // Check args
     if(argc != 2) {
@@ -23,14 +23,14 @@ int main(int argc, char ** argv) {
     }
 
     // Verify port number
-    portno = atoi(argv[0]);
+    portno = strtoul(argv[1], NULL, 10);
     if(portno < 0 || portno > 65535) {
         fprintf(stderr, "invalid port number: %d\n", portno);
         exit(1);
     }
 
     // Create socket 
-    sockfd = socket(PF_INET6, SOCK_STREAM, 0); 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
     if(sockfd < 0) {
         fprintf(stderr, "error (%d) creating socket\n", sockfd);
         exit(2);
@@ -38,12 +38,12 @@ int main(int argc, char ** argv) {
 
     memset((char *) &serv_addr, '\0', sizeof(serv_addr));
     
-    serv_addr.sin6_family = PF_INET6;
-    serv_addr.sin6_addr = in6addr_any;
-    serv_addr.sin6_port = htons(portno);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
     
     // Bind socket to port
-    if(bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+    if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         fprintf(stderr, "error binding socket to port %d\n", portno);
         exit(3); 
     }
@@ -55,20 +55,17 @@ int main(int argc, char ** argv) {
 
     // Wait for & accept incoming connections
     while(1) { 
-        printf("Ayyyy lmao\n");
         int clisockfd, clilen;
         char buffer[BUFFER_SIZE];
     
         clilen = sizeof(cli_addr);
         
         clisockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
- 
+
         if(clisockfd < 0) {
             fprintf(stderr, "error accepting connection\n");
             exit(5);
         }    
-
-        printf("Accepted connection\n");
  
         memset(buffer, '\0', BUFFER_SIZE * sizeof(char));
 
@@ -77,9 +74,9 @@ int main(int argc, char ** argv) {
             exit(6);
         }
 
-        fprintf(stdin, "Here is the message: %s\n", buffer);
+        printf("Here is the message: %s", buffer);
         
-        if(write(clisockfd, "I got your message!", 18) < 0) {
+        if(write(clisockfd, "I got your message!\n", 18) < 0) {
             fprintf(stderr, "error writing to socket\n");
             exit(7);
         }
